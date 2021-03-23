45bbfec3e32ce1630f2b9086eb6932c7be39e75e
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2021-03-23 14:07:00 +0100

    Templated MeshRenderer::set_mesh for more genericity

--- include/rtac_display/renderers/MeshRenderer.h
+++ include/rtac_display/renderers/MeshRenderer.h
@@ -45,12 +45,52 @@ class MeshRenderer : public Renderer
     MeshRenderer(const View3D::Ptr& view,
                  const Color& color = {1.0,1.0,1.0});
 
-    void set_mesh(const Mesh& mesh);
+    //void set_mesh(const Mesh& mesh);
     void set_pose(const Pose& pose);
     void set_color(const Color& color);
 
     virtual void draw();
+    
+    template <typename Tp, typename Tf>
+    void set_mesh(const types::Mesh<Tp,Tf>& mesh);
 };
+
+template <typename Tp, typename Tf>
+void MeshRenderer::set_mesh(const types::Mesh<Tp,Tf>& mesh)
+{
+    using namespace rtac::types;
+    using namespace rtac::types::indexing;
+
+    this->allocate_points(3*mesh.num_faces());
+    
+    glBindBuffer(GL_ARRAY_BUFFER, points_);
+    auto points  = static_cast<float*>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
+    glBindBuffer(GL_ARRAY_BUFFER, normals_);
+    auto normals = static_cast<float*>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
+
+    for(int nf = 0; nf < mesh.num_faces(); nf++) {
+        auto f = mesh.face(nf);
+        Map<const Vector3<float>> p0(reinterpret_cast<const float*>(&mesh.point(f.x)));
+        Map<const Vector3<float>> p1(reinterpret_cast<const float*>(&mesh.point(f.y)));
+        Map<const Vector3<float>> p2(reinterpret_cast<const float*>(&mesh.point(f.z)));
+        Vector3<float> n = ((p1 - p0).cross(p2 - p0)).normalized();
+        
+        int i = 9*nf;
+        points[i]     = p0(0); points[i + 1] = p0(1); points[i + 2] = p0(2);
+        points[i + 3] = p1(0); points[i + 4] = p1(1); points[i + 5] = p1(2);
+        points[i + 6] = p2(0); points[i + 7] = p2(1); points[i + 8] = p2(2);
+        normals[i]     = n(0); normals[i + 1] = n(1); normals[i + 2] = n(2);
+        normals[i + 3] = n(0); normals[i + 4] = n(1); normals[i + 5] = n(2);
+        normals[i + 6] = n(0); normals[i + 7] = n(1); normals[i + 8] = n(2);
+    }
+    
+    glUnmapBuffer(GL_ARRAY_BUFFER);
+    glBindBuffer(GL_ARRAY_BUFFER, points_);
+    glUnmapBuffer(GL_ARRAY_BUFFER);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    numPoints_ = 9*mesh.num_faces();
+}
 
 }; //namespace display
 }; //namespace rtac
--- src/renderers/MeshRenderer.cpp
+++ src/renderers/MeshRenderer.cpp
@@ -80,41 +80,43 @@ void MeshRenderer::delete_points()
     numPoints_ = 0;
 }
 
-void MeshRenderer::set_mesh(const Mesh& mesh)
-{
-    using namespace rtac::types;
-    using namespace rtac::types::indexing;
-
-    this->allocate_points(3*mesh.num_faces());
-    
-    glBindBuffer(GL_ARRAY_BUFFER, points_);
-    auto points  = static_cast<float*>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
-    glBindBuffer(GL_ARRAY_BUFFER, normals_);
-    auto normals = static_cast<float*>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
-
-    for(int nf = 0; nf < mesh.num_faces(); nf++) {
-        auto f = mesh.face(nf);
-        Map<const Vector3<float>> p0(reinterpret_cast<const float*>(&mesh.point(f.x)));
-        Map<const Vector3<float>> p1(reinterpret_cast<const float*>(&mesh.point(f.y)));
-        Map<const Vector3<float>> p2(reinterpret_cast<const float*>(&mesh.point(f.z)));
-        Vector3<float> n = ((p1 - p0).cross(p2 - p0)).normalized();
-        
-        int i = 9*nf;
-        points[i]     = p0(0); points[i + 1] = p0(1); points[i + 2] = p0(2);
-        points[i + 3] = p1(0); points[i + 4] = p1(1); points[i + 5] = p1(2);
-        points[i + 6] = p2(0); points[i + 7] = p2(1); points[i + 8] = p2(2);
-        normals[i]     = n(0); normals[i + 1] = n(1); normals[i + 2] = n(2);
-        normals[i + 3] = n(0); normals[i + 4] = n(1); normals[i + 5] = n(2);
-        normals[i + 6] = n(0); normals[i + 7] = n(1); normals[i + 8] = n(2);
-    }
-    
-    glUnmapBuffer(GL_ARRAY_BUFFER);
-    glBindBuffer(GL_ARRAY_BUFFER, points_);
-    glUnmapBuffer(GL_ARRAY_BUFFER);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    numPoints_ = 9*mesh.num_faces();
-}
+//template <>
+//void MeshRenderer::set_mesh<types::Mesh<>::Point, types::Mesh<>::Face>(
+//    const types::Mesh<>& mesh)
+//{
+//    using namespace rtac::types;
+//    using namespace rtac::types::indexing;
+//
+//    this->allocate_points(3*mesh.num_faces());
+//    
+//    glBindBuffer(GL_ARRAY_BUFFER, points_);
+//    auto points  = static_cast<float*>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
+//    glBindBuffer(GL_ARRAY_BUFFER, normals_);
+//    auto normals = static_cast<float*>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
+//
+//    for(int nf = 0; nf < mesh.num_faces(); nf++) {
+//        auto f = mesh.face(nf);
+//        Map<const Vector3<float>> p0(reinterpret_cast<const float*>(&mesh.point(f.x)));
+//        Map<const Vector3<float>> p1(reinterpret_cast<const float*>(&mesh.point(f.y)));
+//        Map<const Vector3<float>> p2(reinterpret_cast<const float*>(&mesh.point(f.z)));
+//        Vector3<float> n = ((p1 - p0).cross(p2 - p0)).normalized();
+//        
+//        int i = 9*nf;
+//        points[i]     = p0(0); points[i + 1] = p0(1); points[i + 2] = p0(2);
+//        points[i + 3] = p1(0); points[i + 4] = p1(1); points[i + 5] = p1(2);
+//        points[i + 6] = p2(0); points[i + 7] = p2(1); points[i + 8] = p2(2);
+//        normals[i]     = n(0); normals[i + 1] = n(1); normals[i + 2] = n(2);
+//        normals[i + 3] = n(0); normals[i + 4] = n(1); normals[i + 5] = n(2);
+//        normals[i + 6] = n(0); normals[i + 7] = n(1); normals[i + 8] = n(2);
+//    }
+//    
+//    glUnmapBuffer(GL_ARRAY_BUFFER);
+//    glBindBuffer(GL_ARRAY_BUFFER, points_);
+//    glUnmapBuffer(GL_ARRAY_BUFFER);
+//    glBindBuffer(GL_ARRAY_BUFFER, 0);
+//
+//    numPoints_ = 9*mesh.num_faces();
+//}
 
 void MeshRenderer::set_pose(const Pose& pose)
 {
