30f18f2e7e19e70883c03e40cd28c9d60fc7cf6e
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2022-01-11 14:37:37 +0100

    [GLSLType] Added GLSL type deduction from native C types

---
+++ include/rtac_display/GLSLType.h
@@ -0,0 +1,107 @@
+#ifndef _DEF_RTAC_DISPLAY_GLSL_TYPE_H_
+#define _DEF_RTAC_DISPLAY_GLSL_TYPE_H_
+
+#include <type_traits>
+
+#include <rtac_display/GLFormat.h>
+
+namespace rtac { namespace display {
+
+template <unsigned int Size>
+struct GLSLTypeFloat
+{
+    static_assert(Size > 0 && Size <= 4, "Invalid GLSL type size");
+
+    struct Float1 { static constexpr const char* value = "float"; };
+    struct Float2 { static constexpr const char* value = "vec2"; };
+    struct Float3 { static constexpr const char* value = "vec3"; };
+    struct Float4 { static constexpr const char* value = "vec4"; };
+    
+    static constexpr const char* value = std::conditional<Size == 1, Float1,
+                                typename std::conditional<Size == 2, Float2,
+                                typename std::conditional<Size == 3, Float3, Float4
+                                >::type>::type>::type::value;
+};
+
+template <unsigned int Size>
+struct GLSLTypeDouble
+{
+    static_assert(Size > 0 && Size <= 4, "Invalid GLSL type size");
+
+    struct Double1 { static constexpr const char* value = "double"; };
+    struct Double2 { static constexpr const char* value = "dvec2"; };
+    struct Double3 { static constexpr const char* value = "dvec3"; };
+    struct Double4 { static constexpr const char* value = "dvec4"; };
+    
+    static constexpr const char* value = std::conditional<Size == 1, Double1,
+                                typename std::conditional<Size == 2, Double2,
+                                typename std::conditional<Size == 3, Double3, Double4
+                                >::type>::type>::type::value;
+};
+
+template <unsigned int Size>
+struct GLSLTypeInt
+{
+    static_assert(Size > 0 && Size <= 4, "Invalid GLSL type size");
+
+    struct Int1 { static constexpr const char* value = "int"; };
+    struct Int2 { static constexpr const char* value = "ivec2"; };
+    struct Int3 { static constexpr const char* value = "ivec3"; };
+    struct Int4 { static constexpr const char* value = "ivec4"; };
+    
+    static constexpr const char* value = std::conditional<Size == 1, Int1,
+                                typename std::conditional<Size == 2, Int2,
+                                typename std::conditional<Size == 3, Int3, Int4
+                                >::type>::type>::type::value;
+};
+
+template <unsigned int Size>
+struct GLSLTypeUint
+{
+    static_assert(Size > 0 && Size <= 4, "Invalid GLSL type size");
+
+    struct Uint1 { static constexpr const char* value = "uint"; };
+    struct Uint2 { static constexpr const char* value = "uvec2"; };
+    struct Uint3 { static constexpr const char* value = "uvec3"; };
+    struct Uint4 { static constexpr const char* value = "uvec4"; };
+    
+    static constexpr const char* value = std::conditional<Size == 1, Uint1,
+                                typename std::conditional<Size == 2, Uint2,
+                                typename std::conditional<Size == 3, Uint3, Uint4
+                                >::type>::type>::type::value;
+};
+
+/**
+ * The purpose of the GLSLType struct is to infer a GLSL native type string
+ * from a GLFormat type struct.
+ *
+ * This allows to write generic glsl shader code (analogous to C++ templates)
+ * which will be automatically adapted to a user type. (See GLReductor for an
+ * example).
+ */
+template <typename T>
+struct GLSLType
+{
+    using Format                       = GLFormat<T>;
+    using Scalar                       = typename Format::Scalar;
+    static constexpr unsigned int Size = Format::Size;
+
+    static_assert(Size > 0 && Size <= 4, "Invalid GLSL type size");
+    static_assert(std::is_integral<Scalar>::value
+               || std::is_same<Scalar,float>::value
+               || std::is_same<Scalar,double>::value,
+               "Invalid GLSL scalar type");
+
+    static constexpr const char* value = std::conditional<
+        std::is_integral<Scalar>::value,
+            typename std::conditional<std::is_signed<Scalar>::value,
+                GLSLTypeInt<Size>, GLSLTypeUint<Size>>::type,
+            typename std::conditional<std::is_same<Scalar,float>::value,
+                GLSLTypeFloat<Size>, GLSLTypeDouble<Size>>::type
+        >::type::value;
+};
+
+}; //namespace display
+}; //namespace rtac
+
+#endif //_DEF_RTAC_DISPLAY_GLSL_TYPE_H_
---
+++ tests/src/glsl_types.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+#include <rtac_display/GLSLType.h>
+using namespace rtac::display;
+
+using namespace rtac::types;
+
+int main()
+{
+    cout << GLSLTypeFloat<1>::value << endl;
+    cout << GLSLTypeFloat<2>::value << endl;
+    cout << GLSLTypeFloat<3>::value << endl;
+    cout << GLSLTypeFloat<4>::value << endl;
+
+    cout << GLSLTypeDouble<1>::value << endl;
+    cout << GLSLTypeDouble<2>::value << endl;
+    cout << GLSLTypeDouble<3>::value << endl;
+    cout << GLSLTypeDouble<4>::value << endl;
+
+    cout << GLSLTypeInt<1>::value << endl;
+    cout << GLSLTypeInt<2>::value << endl;
+    cout << GLSLTypeInt<3>::value << endl;
+    cout << GLSLTypeInt<4>::value << endl;
+
+    cout << GLSLTypeUint<1>::value << endl;
+    cout << GLSLTypeUint<2>::value << endl;
+    cout << GLSLTypeUint<3>::value << endl;
+    cout << GLSLTypeUint<4>::value << endl;
+    cout << endl << endl;
+
+    cout << GLSLType<float>::value << endl;
+    cout << GLSLType<Point2<float>>::value << endl;
+    cout << GLSLType<Point3<float>>::value << endl;
+    cout << GLSLType<Point4<float>>::value << endl;
+
+    cout << GLSLType<double>::value << endl;
+    cout << GLSLType<Point2<double>>::value << endl;
+    cout << GLSLType<Point3<double>>::value << endl;
+    cout << GLSLType<Point4<double>>::value << endl;
+
+    cout << GLSLType<unsigned char>::value << endl;
+    cout << GLSLType<Point2<unsigned char>>::value << endl;
+    cout << GLSLType<Point3<unsigned char>>::value << endl;
+    cout << GLSLType<Point4<unsigned char>>::value << endl;
+
+    cout << GLSLType<uint32_t>::value << endl;
+    cout << GLSLType<Point2<uint32_t>>::value << endl;
+    cout << GLSLType<Point3<uint32_t>>::value << endl;
+    cout << GLSLType<Point4<uint32_t>>::value << endl;
+
+    cout << GLSLType<char>::value << endl;
+    cout << GLSLType<Point2<char>>::value << endl;
+    cout << GLSLType<Point3<char>>::value << endl;
+    cout << GLSLType<Point4<char>>::value << endl;
+
+    cout << GLSLType<int32_t>::value << endl;
+    cout << GLSLType<Point2<int32_t>>::value << endl;
+    cout << GLSLType<Point3<int32_t>>::value << endl;
+    cout << GLSLType<Point4<int32_t>>::value << endl;
+
+    return 0;
+}
